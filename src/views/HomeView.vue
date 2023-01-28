84f6a76891126698439a2574e35cae8554891861
ricardsupenieks <upenieksrica@gmail.com>
2023-01-28 21:51:51 +0200

    filtering added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,27 @@
 
             <h1 class="text-5xl font-bold text-white text-center rounded-lg">todo list</h1>
 
-            <div class="text-white flex flex-row justify-center gap-2">
-                <button>All</button>
-                <button>Active</button>
-                <button>Complete</button>
+            <div class="text-white flex flex-row justify-center gap-2 text-lg">
+                <button
+                    class="focus:shadow-2xl focus:underline decoration-2"
+                    @click="state.filter = 'all'"
+                >
+                    All
+                </button>
+
+                <button
+                    class="focus:shadow-2xl focus:underline decoration-2"
+                    @click="state.filter = 'active'"
+                >
+                    Active
+                </button>
+
+                <button
+                    class="focus:shadow-2xl focus:underline decoration-2"
+                    @click="state.filter = 'completed'"
+                >
+                    Completed
+                </button>
             </div>
 
             <div class="relative mt-4">
@@ -23,15 +40,44 @@
             </div>
 
             <div class="mt-10 max-h-52 overflow-auto">
-                
                 <div
-                v-for="(todo, index) in state.todos"
-                :key="index"
-                class="bg-opacity-80 bg-white"
-                >
-                    <p class="p-1 text-stone-700 flex flex-row justify-between gap-3">
-                        <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)"/> 
-                    </p>
+                    v-if="state.filter === 'all'"
+                    v-for="(todo, index) in state.todos"
+                    :key="index"
+                    class="bg-opacity-80 bg-white"
+                    >
+                        <div class="p-1 text-stone-700 flex flex-row justify-between gap-3">
+
+                            <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)"/> 
+                        </div>
+                </div>
+
+                <div
+                    v-if="state.filter === 'active'"
+                    v-for="(todo, index) in activeTasks"
+                    :key="index"
+                    class="bg-opacity-80 bg-white"
+                    >
+                        <div 
+                            class="p-1 text-stone-700 flex flex-row justify-between gap-3 cursor-pointer"
+                            @click="todo.isCompleted = true"
+                            >
+                            <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)"/> 
+                        </div>
+                </div>
+
+                <div
+                    v-if="state.filter === 'completed'"
+                    v-for="(todo, index) in completedTasks"
+                    :key="index"
+                    class="bg-opacity-80 bg-white"
+                    >
+                        <div 
+                            class="p-1 text-stone-700 flex flex-row justify-between gap-3 cursor-pointer"
+                            @click="todo.isCompleted = false"
+                        >
+                            <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)"/> 
+                        </div>
                 </div>
             </div>
         </div>
@@ -40,12 +86,13 @@
 
 <script setup lang="ts">
 import { reactive } from 'vue';
+import { computed } from 'vue';
 import TodoItem from '../components/TodoItem.vue'
 
 interface Todo {
     task: string;
     isCompleted: boolean;
-}
+};
 
 const state = reactive({
     todos: [
@@ -58,7 +105,31 @@ const state = reactive({
     todo: "",
 
     filter: "all",
-})
+});
+
+const completedTasks = computed(() => {
+    const tasks: Todo[] = [];
+
+    state.todos.forEach(todo => {
+        if(todo.isCompleted) {
+            tasks.push(todo);
+        }
+    });
+
+    return tasks;
+});
+
+const activeTasks = computed(() => {
+    const tasks: Todo[] = [];
+
+    state.todos.forEach(todo => {
+        if(!todo.isCompleted) {
+            tasks.push(todo);
+        }
+    });
+
+    return tasks;
+});
 
 function addTodo() {
     const newTodo = {
@@ -67,11 +138,11 @@ function addTodo() {
     };
     state.todos.push(newTodo);
     state.todo="";
-}
+};
 
 function deleteTodo(index: number) {
     state.todos.splice(index, 1);
     state.todo="";
-}
+};
 
 </script>
