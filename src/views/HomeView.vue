4f75f6ac6883b3f98176669a1d84b9f7feee1440
ricardsupenieks <upenieksrica@gmail.com>
2023-01-28 22:48:50 +0200

    refactored filtering

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,131 +1,65 @@
 <template>
     <div class="flex flex-col h-screen place-items-center">
-        <div class="border-8 p-16 items-center m-auto bg-gradient-to-b from-cyan-400 to-yellow-100/80 shadow-lg h-[530px]" style="background-image: url(https://i.gifer.com/origin/7c/7c3b7a0f3dec2723c1be12064a5002bc.gif);">
+        <div class="border-8 p-16 items-center m-auto shadow-lg h-[530px]"
+            style="background-image: url(https://i.gifer.com/origin/7c/7c3b7a0f3dec2723c1be12064a5002bc.gif);">
 
             <h1 class="text-5xl font-bold text-white text-center rounded-lg">todo list</h1>
 
-            <div 
-                v-if="state.filter === 'all'"
-                class="text-white flex flex-row justify-center gap-2 text-lg"
-                >
-                <button
-                    class="focus:shadow-2xl underline decoration-2"
-                    @click="state.filter = 'all'"
-                >
+            <div v-if="state.filter === 'all'" class="text-white flex flex-row justify-center gap-2 text-lg">
+                <button class="focus:shadow-2xl underline decoration-2" @click="state.filter = 'all'">
                     All
                 </button>
 
-                <button
-                    @click="state.filter = 'active'"
-                >
+                <button @click="state.filter = 'active'"> 
                     Active
                 </button>
 
-                <button
-                    @click="state.filter = 'completed'"
-                >
+                <button @click="state.filter = 'completed'">
                     Completed
                 </button>
             </div>
 
-            <div 
-                v-if="state.filter === 'active'"
-                class="text-white flex flex-row justify-center gap-2 text-lg"
-                >
-                <button
-                    @click="state.filter = 'all'"
-                >
+            <div v-if="state.filter === 'active'" class="text-white flex flex-row justify-center gap-2 text-lg">
+                <button @click="state.filter = 'all'">
                     All
                 </button>
 
-                <button
-                    @click="state.filter = 'active'"
-                    class="focus:shadow-2xl underline decoration-2"
-                >
+                <button @click="state.filter = 'active'" class="focus:shadow-2xl underline decoration-2">
                     Active
                 </button>
 
-                <button
-                    @click="state.filter = 'completed'"
-                >
+                <button @click="state.filter = 'completed'">
                     Completed
                 </button>
             </div>
 
-            <div 
-                v-if="state.filter === 'completed'"
-                class="text-white flex flex-row justify-center gap-2 text-lg"
-                >
-                <button
-                    @click="state.filter = 'all'"
-                >
+            <div v-if="state.filter === 'completed'" class="text-white flex flex-row justify-center gap-2 text-lg">
+                <button @click="state.filter = 'all'">
                     All
                 </button>
 
-                <button
-                    @click="state.filter = 'active'"
-                >
+                <button @click="state.filter = 'active'">
                     Active
                 </button>
 
-                <button
-                    @click="state.filter = 'completed'"
-                    class="focus:shadow-2xl underline decoration-2"
-                >
+                <button @click="state.filter = 'completed'" class="focus:shadow-2xl underline decoration-2">
                     Completed
                 </button>
             </div>
 
             <div class="relative mt-4">
-                <input
-                    type="text"
-                    v-model="state.todo"
+                <input type="text" v-model="state.todo"
                     class="block w-full p-3 border-2 bg-opacity-80 rounded-lg bg-white shadow-xl outline-none text-stone-700"
-                    placeholder="Add todo..."
-                    maxlength="50"
-                    @keyup.enter="addTodo()"
-                    />
+                    placeholder="Add todo..." maxlength="50" @keyup.enter="addTodo()" />
             </div>
 
             <div class="mt-10 max-h-52 overflow-auto">
-                <div
-                    v-if="state.filter === 'all'"
-                    v-for="(todo, index) in state.todos"
-                    :key="index"
-                    class="bg-opacity-80 bg-white rounded"
-                    >
-                        <div class="p-1 text-stone-700 flex flex-row justify-between gap-3">
+                <div v-for="(todo, index) in filteredTasks" :key="index"
+                    class="bg-opacity-80 bg-white rounded">
+                    <div class="p-1 text-stone-700 flex flex-row justify-between gap-3">
 
-                            <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)"/> 
-                        </div>
-                </div>
-
-                <div
-                    v-if="state.filter === 'active'"
-                    v-for="(todo, index) in activeTasks"
-                    :key="index"
-                    class="bg-opacity-80 bg-white rounded"
-                    >
-                        <div 
-                            class="p-1 text-stone-700 flex flex-row justify-between gap-3 cursor-pointer"
-                            @click="todo.isCompleted = true"
-                            >
-                            <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)"/> 
-                        </div>
-                </div>
-
-                <div
-                    v-if="state.filter === 'completed'"
-                    v-for="(todo, index) in completedTasks"
-                    :key="index"
-                    class="bg-opacity-80 bg-white rounded"
-                    >
-                        <div 
-                            class="p-1 text-stone-700 flex flex-row justify-between gap-3 cursor-pointer"
-                            @click="todo.isCompleted = false"
-                        >
-                            <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)"/> 
-                        </div>
+                        <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -155,42 +89,28 @@ const state = reactive({
     filter: "all",
 });
 
-const completedTasks = computed((): Todo[] => {
-    const tasks: Todo[] = [];
-
-    state.todos.forEach(todo => {
-        if(todo.isCompleted) {
-            tasks.push(todo);
-        }
-    });
-
-    return tasks;
-});
-
-const activeTasks = computed((): Todo[] => {
-    const tasks: Todo[] = [];
-
-    state.todos.forEach(todo => {
-        if(!todo.isCompleted) {
-            tasks.push(todo);
-        }
-    });
-
-    return tasks;
-});
+const filteredTasks = computed((): Todo[] => {
+    switch(state.filter) {
+        case 'completed':
+            return state.todos.filter(todo => todo.isCompleted);
+        case 'active':
+            return state.todos.filter(todo => !todo.isCompleted); 
+    }
+    return state.todos;
+})
 
 function addTodo(): void {
     const newTodo = {
         task: state.todo,
-        isCompleted: false 
+        isCompleted: false
     };
     state.todos.push(newTodo);
-    state.todo="";
+    state.todo = "";
 };
 
 function deleteTodo(index: number): void {
     state.todos.splice(index, 1);
-    state.todo="";
+    state.todo = "";
 };
 
 </script>
