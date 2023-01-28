8891ee5286810acba8303d774efba860799f998d
ricardsupenieks <upenieksrica@gmail.com>
2023-01-28 20:43:03 +0200

    todo list progress, deleted unneccesary files

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>{{ index + 1}}</p> 
+    <p>{{ task }}</p>
+    <button @click="$emit('deleteTodo')">X</button>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+    task: string,
+    index: number,
+}
+
+interface Emits {
+    (e: "deleteTodo"): void;
+}
+
+const props = defineProps<Props>();
+
+const emit = defineEmits<Emits>();
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,97 +1,77 @@
 <template>
-    <div class="bg-blue-300">Hello</div>
-</template>
+    <div class="flex flex-col h-screen place-items-center">
+        <div class="border-8 p-16 items-center m-auto bg-gradient-to-b from-cyan-400 to-yellow-100/80 shadow-lg h-[530px]" style="background-image: url(https://i.gifer.com/origin/7c/7c3b7a0f3dec2723c1be12064a5002bc.gif);">
 
-<script setup lang="ts">
-import "@/assets/index.css";
-import { reactive } from 'vue';
+            <h1 class="text-5xl font-bold text-white text-center rounded-lg">todo list</h1>
 
+            <div class="text-white flex flex-row justify-center gap-2">
+                <button>All</button>
+                <button>Active</button>
+                <button>Complete</button>
+            </div>
 
-const state = reactive({
-    todos: [],
-})
+            <div class="relative mt-4">
+                <input
+                    type="text"
+                    v-model="state.todo"
+                    class="block w-full p-3 border-2 bg-opacity-80 rounded-lg bg-white shadow-xl outline-none text-stone-700"
+                    placeholder="Add todo..."
+                    maxlength="50"
+                    @keyup.enter="addTodo()"
+                    />
+                   
+            </div>
 
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- INPUT -->
-<!-- <template>
-    <div>Input</div>
-    <input v-model="state.inputValue"/>
-
-    <div class="input">{{ state.inputValue }}</div>
-</template>
-
-<script setup lang="ts">
-import { reactive } from 'vue';
-
-
-const state = reactive({
-    inputValue: "",
-})
-
-// function updateInput(input: any) {
-//     state.inputValue = input.target.value;
-// }
-
-</script>
-
-<style>
-.counter {
-    background-color: aqua;
-    font-size: 30px;
-    text-align: ce;
-    padding: 10px;
-}
-</style> -->
-
-
-
-<!-- COUNTER  -->
-<!-- <template>
-    <div>Counter</div>
-    <button @click="incrementCounter(1)">Increase</button>
-    <button @click="incrementCounter(-1)">Decrease</button>
-    <div class="counter">{{ state.counter }}</div>
+            <div class="mt-10 max-h-52 overflow-auto">
+                
+                <div
+                v-for="(todo, index) in state.todos"
+                :key="index"
+                class="bg-opacity-80 bg-white"
+                >
+                    <p class="p-1 text-stone-700 flex flex-row justify-between gap-3">
+                        <TodoItem :index="index" :task="todo.task" @deleteTodo="deleteTodo(index)"/> 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue';
+import TodoItem from '../components/TodoItem.vue'
 
+interface Todo {
+    task: string;
+    isCompleted: boolean;
+}
 
 const state = reactive({
-    counter: 0,
+    todos: [
+        {
+            task: "Make todo list",
+            isCompleted: false,
+        }
+    ] as Todo[],
+
+    todo: "",
+
+    filter: "all",
 })
 
-function incrementCounter(value: number) {
-    state.counter+=value;
+function addTodo() {
+    const newTodo = {
+        task: state.todo,
+        isCompleted: false 
+    };
+    state.todos.push(newTodo);
+    state.todo="";
+}
+
+function deleteTodo(index: number) {
+    state.todos.splice(index, 1);
+    state.todo="";
 }
 
 </script>
-
-<style>
-.counter {
-    background-color: aqua;
-    font-size: 30px;
-    text-align: ce;
-    padding: 10px;
-}
-</style> -->
